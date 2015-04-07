3920dfba32d43cc4e0290355f029a86b3f3d7d90
Charlie Depman <cdepman@gmail.com>
2015-04-07 08:53:25 +0300

    adds longest_word

---
+++ longest_word/longest_word.rb
@@ -0,0 +1,15 @@
+# LONGEST WORD
+# LANGUAGE: RUBY
+#
+# Create a function longest_word(sentence) that takes the sentence parameter being passed and returns the 
+# longest word in the string. If there are two or more words that are the same length, return the first word from the 
+# string with that length. Ignore punctuation and assume the sentence will not be empty. 
+
+# Examples: 
+# longest_word("There are many hippopotami living in Naivasha Lake") # hippopotami
+# longest_word("Watamu has great fresh seafood") # seafood
+# longest_word("The moon and stars shine over the veldt") # stars
+
+def longest_word(sentence)
+  # Your Code Here!
+end
