253c5583714cc9d6a90a0c2b96e0c54798c115cd
Charlie Depman <cdepman@gmail.com>
2015-04-13 10:04:28 +0300

    edits even_occurence

---
+++ even_occurence/even_occurance.rb
@@ -0,0 +1,15 @@
+# EVEN OCCURANCE
+# LANGUAGE: RUBY
+#
+# Find the first item that occurs an even number of times in an array.
+# Remember to handle multiple even-occurrence items and return the first one. 
+# Return nil if there are no even-occurrence items.
+
+# example usage:
+# even_occurrence([1, 7, 2, 4, 5, 6, 8, 6, 9, 6, 4]) # 4
+# even_occurrence([1, 2, 3, 4, 3, 1, 1]) # 3
+# even_occurrence([1,2,3]) #nil
+
+def even_occurence(num_array)
+  # Your Code here!
+end
